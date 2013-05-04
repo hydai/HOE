f48d87295e29424a218b3e07db3cb2441f2a435e
Jack Force <jack1243star@gmail.com>
2013-05-04 17:52:14 +0800

    Convert indentation to tabs

--- include/special_product.hpp
+++ include/special_product.hpp
@@ -9,31 +9,31 @@
 #include "CostFunction.hpp"
 
 enum SpecialProductType {
-    QUARRY,
-    IRON, 
-    CRYSTAL, 
-    SULFUR
+	QUARRY,
+	IRON, 
+	CRYSTAL, 
+	SULFUR
 };
 
 class SpecialProduct {
-    /* number of the player have */
-    int num_holdings;
-    /* use the cost functions mathod */
-    friend class CostFunction;
-    /* use the earn source mathod */
-    friend class EarnSource;
-    /* type of this specital product */
-    enum SpecialProductType type_of_SP;
+	/* number of the player have */
+	int num_holdings;
+	/* use the cost functions mathod */
+	friend class CostFunction;
+	/* use the earn source mathod */
+	friend class EarnSource;
+	/* type of this specital product */
+	enum SpecialProductType type_of_SP;
 
 public:
-    /* constructor */
-    SpecialProduct(enum SpecialProductType );
-    /* destructor */
-    ~SpecialProduct(void);
-    /* get total number of the holdings */
-    int getHoldingsNumber(void);
-    /* get type name */
-    const char* getTypeName(void);
+	/* constructor */
+	SpecialProduct(enum SpecialProductType );
+	/* destructor */
+	~SpecialProduct(void);
+	/* get total number of the holdings */
+	int getHoldingsNumber(void);
+	/* get type name */
+	const char* getTypeName(void);
 };
 
 #endif
--- src/special_product.cpp
+++ src/special_product.cpp
@@ -9,34 +9,34 @@
 
 /* constructor */
 SpecialProduct::SpecialProduct(enum SpecialProductType _type) {
-    this->num_holdings = 0;
-    type_of_SP = _type;
+	this->num_holdings = 0;
+	type_of_SP = _type;
 }
 
 /* destructor */
 SpecialProduct::~SpecialProduct(void) {
-    this->num_holdings = 0;
+	this->num_holdings = 0;
 }
 
 const char* SpecialProduct::getTypeName(void) {
-    switch (type_of_SP) {
-    case 0:
-        return "QUARRY";
-    case 1:
-        return "IRON";
-    case 2:
-        return "CRYSTAL";
-    case 3:
-        return "SULFUR";
-    default:
-        return "ERROR";
-    }
-    return "ERROR";
+	switch (type_of_SP) {
+	case 0:
+		return "QUARRY";
+	case 1:
+		return "IRON";
+	case 2:
+		return "CRYSTAL";
+	case 3:
+		return "SULFUR";
+	default:
+		return "ERROR";
+	}
+	return "ERROR";
 }
 
 int SpecialProduct::getHoldingsNumber(void) {
-    char *TypeName = new [strlen(getTypeName())+1];
-    std::strcpy(TypeName, getTypeName());
-    std::printf("This is %s, and have %d\n", TypeName, num_holdings);
-    delete TypeName;
+	char *TypeName = new [strlen(getTypeName())+1];
+	std::strcpy(TypeName, getTypeName());
+	std::printf("This is %s, and have %d\n", TypeName, num_holdings);
+	delete TypeName;
 }
